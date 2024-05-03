e6596b0b86914366904fc78708edb92b357efad2
dorcas Chepkirui <dorcaschepkirui22@gmail.com>
2024-05-03 14:34:33 +0300

    new commits

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useState, useEffect } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
+  const login = () =>{
+    setIsLoggedIn(true);
+  }
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+  useEffect(() =>{
+    if (isLoggedIn) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, [isLoggedIn]);
 
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      <NavBar logout={logout}/>
+      { isLoggedIn ? <NavBar logout={logout} /> : <Navigate to="/login" />}
+      <Outlet context={login} />
     </div>
   );
 }
